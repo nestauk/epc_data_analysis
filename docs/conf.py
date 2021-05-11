2522d3a4cdcac49e80eb913533c5bb6ef7e764b2
Julia Suter <juliasuter@NESTA-FVFF81W0Q05P.local>
2021-05-11 17:27:59 +0100

    Setup Nesta Data Science cookiecutter

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+"""Sphinx configuration."""
+from datetime import datetime
+
+
+project = "EPC_data_analysis"
+author = "Julia Suter"
+copyright = f"{datetime.now().year}, {author}"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    # "sphinx_click",
+    "sphinx_rtd_theme",
+]
+autodoc_typehints = "description"
+html_theme = "sphinx_rtd_theme"
