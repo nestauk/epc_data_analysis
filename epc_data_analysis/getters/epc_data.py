3d920b463b9c8edde09f46b9d1a58e7dd201431b
Julia Suter <juliasuter@NESTA-FVFF81W0Q05P.local>
2021-07-15 15:46:08 +0100

    Update and comment data getter (#1)

--- epc_data_analysis/getters/epc_data.py
+++ epc_data_analysis/getters/epc_data.py
@@ -1,80 +1,112 @@
+# File: getters/epc_data.py
+""""
+Created May 2021
+@author: Julia Suter
+Last updated on 15/07/2021
+"""
+
+# ---------------------------------------------------------------------------------
+
 import pandas as pd
 import os
-from dask import delayed
-import dask.dataframe as dd
-
 
 from epc_data_analysis import get_yaml_config, Path, PROJECT_DIR
 
+# ---------------------------------------------------------------------------------
 
 # Load config file
 epc_data_config = get_yaml_config(
     Path(str(PROJECT_DIR) + "/epc_data_analysis/config/base.yaml")
 )
 
-# Get paths
+# Get path
 epc_data_path = str(PROJECT_DIR) + epc_data_config["EPC_DATASET_PATH"]
 
 
-def load_EPC_data(cert_subset="all", usecols=None):
+def load_EPC_data(subset="all", usecols=None, low_memory=False):
     """Load and return EPC dataset, or specific subset, as pandas dataframe.
 
-    Return:
-        EPC_certs (pandas dataframe): EPC certificate data.
-    """
+    Parameters
+    ----------
+        subset: 'all', 'Wales', 'England', default='all'
+        EPC certificate area subset.
 
+        usecols: list, default=None
+        List of features/columns to load from EPC dataset.
+
+        low_memory: bool, default=False
+        Internally process the file in chunks, resulting in lower memory use while parsing,
+        but possibly mixed type inference.
+        To ensure no mixed types either set False, or specify the type with the dtype parameter.
+
+    Return
+    ---------
+        EPC_certs: pandas.DateFrame
+        EPC certificate data for given area and features."""
+
+    # Load data
     sample_file_path = epc_data_path + "/domestic-W06000015-Cardiff/certificates.csv"
     sample_df = pd.read_csv(sample_file_path)
 
+    # Get all columns if not other specified
     if usecols == None:
         usecols = sample_df.columns
 
-    if cert_subset == "Wales":
+    # Get Wales data
+    if subset == "Wales":
 
+        # Load specific location certificates and features
         EPC_certs = [
             pd.read_csv(
                 epc_data_path + directory + "/certificates.csv",
-                low_memory=False,
+                low_memory=low_memory,
                 usecols=usecols,
             )
             for directory in os.listdir(epc_data_path)
             if directory.startswith("domestic-W")  # only Wales data
         ]
 
-    elif cert_subset == "England":
+    # Get England data
+    elif subset == "England":
 
+        # Load specific location certificates and features
         EPC_certs = [
             pd.read_csv(
                 epc_data_path + directory + "/certificates.csv",
-                low_memory=False,
+                low_memory=low_memory,
                 usecols=usecols,
             )
             for directory in os.listdir(epc_data_path)
             if directory.startswith("domestic-E")  # only England data
         ]
 
-    elif cert_subset == "all":
+    # Get all data
+    elif subset == "all":
 
-        epc_files = [file for file in os.listdir(epc_data_path)]
+        # Load
         epc_files = [
             file
-            for file in epc_files
+            for file in os.listdir(epc_data_path)
             if not file.startswith(".") and file != "LICENCE.txt"
         ]
+        # epc_files = [
+        # file
+        # for file in epc_files
+        #  if not file.startswith(".") and file != "LICENCE.txt"
+        # ]
 
+        # Load specific location certificates and features
         EPC_certs = [
             pd.read_csv(
                 epc_data_path + directory + "/certificates.csv",
-                low_memory=False,
+                low_memory=low_memory,
                 usecols=usecols,
             )
             for directory in epc_files  # all data (except hidden folders)
         ]
 
     else:
-        raise IOError(
-            "'{}' is not a valid subset of the EPC dataset.".format(cert_subset)
-        )
+        raise IOError("'{}' is not a valid subset of the EPC dataset.".format(subset))
 
     # Concatenate single dataframes into dataframe
     EPC_certs = pd.concat(EPC_certs, axis=0)
@@ -82,13 +114,16 @@ def load_EPC_data(cert_subset="all", usecols=None):
     return EPC_certs
 
 
+# ---------------------------------------------------------------------------------
+
+
 def main():
     """Main function for testing."""
 
-    epc_df = load_EPC_data(cert_subset="Wales")
+    epc_df = load_EPC_data(subset="Wales")
     print(epc_df.head())
 
 
 if __name__ == "__main__":
-    # execute only if run as a script
+    # Execute only if run as a script
     main()
--- epc_data_analysis/getters/util_data.py
+++ epc_data_analysis/getters/util_data.py
@@ -4,31 +4,88 @@ import os
 from epc_data_analysis import get_yaml_config, Path, PROJECT_DIR
 
 # Load config file
-epc_data_config = get_yaml_config(
+data_config = get_yaml_config(
     Path(str(PROJECT_DIR) + "/epc_data_analysis/config/base.yaml")
 )
 
 # Get paths
-postcode_path = str(PROJECT_DIR) + epc_data_config["POSTCODE_PATH"]
+location_path = str(PROJECT_DIR) + data_config["POSTCODE_PATH"]
+WIMD_path = str(PROJECT_DIR) + data_config["WIMD_PATH"]
 
 
-def get_postcode_loc_df() -> pd.DataFrame:
-    """Load and return EPC dataset, or specific subset, as pandas dataframe.
+def get_location_data():
+    """Load location data (postcode, latitude, longitude).
 
-    Return:
-        EPC_certs (pandas dataframe): EPC certificate data.
+    Parameters
+    ----------
+        None
+
+    Return
+    ---------
+        location_data_df: pandas.DateFrame
+        Location data (postcode, latitude, longitude).
     """
 
-    postcode_loc_df = pd.read_csv(postcode_path)
+    # Load data
+    location_data_df = pd.read_csv(location_path)
 
-    del postcode_loc_df["id"]
+    # Remove ID (not necessary and conflicts with EPC dataframe)
+    del location_data_df["id"]
 
-    # Rename column with postcodes
-    postcode_loc_df = postcode_loc_df.rename(
+    # Rename columns to match EPC data
+    location_data_df = location_data_df.rename(
         columns={
             "postcode": "POSTCODE",
             "latitude": "LATITUDE",
             "longitude": "LONGITUDE",
         }
     )
-    return postcode_loc_df
+    return location_data_df
+
+
+def get_WIMD_data():
+    """Load Wales Index of Multiple Deprivation (WIMD).
+
+    Parameters
+    ----------
+        None
+
+    Return
+    ---------
+        wimd_df: pandas.DateFrame
+        Wales Index of Multiple Deprivation data."""
+
+    # Load data
+    wimd_df = pd.read_csv(WIMD_path)
+    return wimd_df
+
+
+def merge_dataframes(df1, df2, merge_feature):
+    """Merge dataframes based on given feature/column.
+
+    Parameters
+    ----------
+        df1: pandas.Dataframe
+        First dataframe to be merged with second dataframe.
+
+        df2: pandas.Dataframe
+        Second dataframe to be merged with first dataframe.
+
+        merge_feature: string
+        Feature/column used to merge two dataframes.
+
+    Return
+    ---------
+        merged_df: pandas.DateFrame
+        Merged new dataframe."""
+
+    # Fix postcode format before merging
+    if merge_feature == "POSTCODE":
+
+        df1["POSTCODE"] = df1["POSTCODE"].str.replace(r" ", "")
+        df2["POSTCODE"] = df2["POSTCODE"].str.replace(r" ", "")
+
+    # Merge datasets
+    merged_df = pd.merge(df1, df2, on=[merge_feature])
+
+    return merged_df
